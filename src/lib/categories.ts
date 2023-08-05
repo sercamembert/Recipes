afe41459eadc3dfc7c043c68d239182486edec31
Filip Maszota <filipmaszota3@gmail.com>
2023-08-05 18:53:56 +0200

    create category schema

---
+++ src/lib/categories.ts
@@ -0,0 +1,44 @@
+interface CategoryData {
+  [key: string]: string[];
+}
+
+export const categories: CategoryData = {
+  Main: [
+    "Fish Dishes",
+    "Meat Dishes",
+    "Vegetarian Dishes",
+    "Chicken Dishes",
+    "Beef Dishes",
+    "Pork Dishes",
+  ],
+  Soups: [
+    "Cream Soups",
+    "Vegetable Soups",
+    "Meat Soups",
+    "Fish Soups",
+    "Cold Soups",
+  ],
+  Salads: ["Vegetable Salads", "Fruit Salads", "Pasta Salads", "Meat Salads"],
+  Snacks: [
+    "Meat Snacks",
+    "Vegetarian Snacks",
+    "Bread Snacks",
+    "Puff Pastry Snacks",
+    "Sweet Snacks",
+  ],
+  Desserts: [
+    "Fruit Desserts",
+    "Chocolate Desserts",
+    "Ice Cream Desserts",
+    "Milk-based Desserts",
+    "No-Bake Desserts",
+  ],
+  Cakes: [
+    "Traditional Cakes",
+    "Chocolate Cakes",
+    "Fruit Cakes",
+    "Gluten-Free Cakes",
+    "No-Bake Cakes",
+    "Tortes",
+  ],
+};
