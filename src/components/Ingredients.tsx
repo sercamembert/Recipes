907d72cd6e6c5bb68aa955db73f2d0e8f945e512
Filip Maszota <filipmaszota3@gmail.com>
2023-08-06 14:06:13 +0200

    add displaying recipe ingredients

---
+++ src/components/Ingredients.tsx
@@ -0,0 +1,33 @@
+import { Ingredient } from "@prisma/client";
+import React from "react";
+import { Checkbox } from "./ui/Checkbox";
+
+interface Props {
+  ingredients: Ingredient[];
+}
+
+const Ingredients = ({ ingredients }: Props) => {
+  return (
+    <div className="w-full lg:w-[75%] flex flex-col gap-4">
+      <h1 className="text-2xl font-secoundary">Ingredints</h1>
+      {ingredients.map((ingredient, index) => (
+        <div
+          key={index}
+          className="w-full items-center flex justify-between border-b border-dark px-2 mb-4 pb-1"
+        >
+          <div className="flex gap-1 items-center">
+            <Checkbox />
+            <p>{ingredient.name}</p>
+          </div>
+          <div className="w-[90px] flex items-start">
+            <p>
+              {ingredient.amount} {ingredient.unit}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Ingredients;
