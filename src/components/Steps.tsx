26b2df41072ae06561cfda4d819df6ba8885854b
Filip Maszota <filipmaszota3@gmail.com>
2023-08-06 14:06:01 +0200

    add recipe steps

---
+++ src/components/Steps.tsx
@@ -0,0 +1,23 @@
+import { Step } from "@prisma/client";
+import React from "react";
+
+interface Props {
+  steps: Step[];
+}
+
+const Steps = ({ steps }: Props) => {
+  return (
+    <div className="w-full flex flex-col gap-4">
+      <h1 className="text-2xl font-secoundary">Instructions</h1>
+
+      {steps.map((step, index) => (
+        <div key={index} className="flex flex-col gap-1">
+          <h1 className="text-xl font-semibold">Step {index + 1}</h1>
+          <p className="text-lg">{step.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Steps;
