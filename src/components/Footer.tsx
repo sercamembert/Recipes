5d346ddb1fb2c779a0ebfb4430f058fa75a36755
Filip Maszota <filipmaszota3@gmail.com>
2023-08-07 12:05:34 +0200

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import React from "react";
+import Logo from "./ui/Logo";
+
+interface Props {}
+
+const Footer = () => {
+  return (
+    <footer className="bg-green-50 w-full absolute left-0 ">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <Logo />
+
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span>© 2023 Recipes™. All Rights Reserved.</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
