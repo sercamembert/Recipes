e543b87e41416d54d49bc39b2e005746791528c9
Filip Maszota <filipmaszota3@gmail.com>
2023-08-15 16:31:47 +0200

    fix footer bug in auth pages

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -3,8 +3,8 @@ import SignIn from "@/components/SignIn";
 
 const page: FC = () => {
   return (
-    <div className="absolute inset-0">
-      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20 ">
+    <div className="h-screen ">
+      <div className="h-full max-w-2xl mx-auto flex  items-center justify-center  ">
         <SignIn />
       </div>
     </div>
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,8 +3,8 @@ import SignUp from "@/components/SignUp";
 
 const page: FC = () => {
   return (
-    <div className="absolute inset-0">
-      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20 ">
+    <div className="h-screen ">
+      <div className="h-full max-w-2xl mx-auto flex  items-center justify-center  ">
         <SignUp />
       </div>
     </div>
