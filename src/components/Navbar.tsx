4699438a12ecc186bd6cbdd0830d7737efacfd60
Filip Maszota <filipmaszota3@gmail.com>
2023-08-20 10:51:11 +0200

    Add searchbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Logo from "./ui/Logo";
-import { Facebook, Instagram, Menu, Twitter, Youtube } from "lucide-react";
-import Link from "next/link";
 import Searchbar from "./Searchbar";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/Sheet";
 import Categories from "./NavCategories";
 import NavbarUser from "./NavbarUser";
 import { getAuthSession } from "@/lib/auth";
@@ -15,13 +12,13 @@ const Navbar = async () => {
   const session = await getAuthSession();
 
   return (
-    <div className="h-16 lg:h-20 py-2 flex items-center fixed top-0 z-10 left-0 right-0 px-[15px] lg:px-[5vw] 2xl:px-[9vw] bg-green-50">
+    <div className="w-full h-16 lg:h-20 py-2 flex items-center fixed top-0 z-10 left-0 right-0 px-[15px] lg:px-[5vw] 2xl:px-[9vw] bg-green-50">
       <Logo />
 
       <Categories />
 
       {/* TODO: Searchbar */}
-      {/* <Searchbar /> */}
+      <Searchbar />
 
       <NavbarUser user={session?.user} />
 
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,17 +1,49 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Command, CommandInput } from "./ui/Command";
+import { CommandList } from "cmdk";
+import { Input } from "./ui/Input";
+import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface SearchbarProps {}
 
 const Searchbar: FC<SearchbarProps> = ({}) => {
+  const [input, setInput] = useState<string>("");
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    router.push(`/search/${input}`);
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  const router = useRouter();
+
   return (
-    <Command className="relative w-[300px] rounded-lg border z-50 overflow-visible">
-      <CommandInput
+    <div className="relative ml-2">
+      <Input
+        type="text"
         placeholder="Search recipes..."
-        className="outline-none border-none focus:border-none focus:outline-none ring-0"
+        className="mx-auto w-full sm:w-[300px] lg:w-[400px] rounded-md border-primary border focus:outline-none ring-0 pr-10"
+        value={input}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
       />
-    </Command>
+      <div className="absolute right-2 top-1/2 transform -translate-y-1/2 ">
+        <Search
+          className="h-5 w-5 hover:text-rose cursor-pointer"
+          onClick={handleSearch}
+        />
+      </div>
+    </div>
   );
 };
 
