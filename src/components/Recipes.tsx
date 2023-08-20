12813bb57acc32412124391d4072245b9bfd8560
Filip Maszota <filipmaszota3@gmail.com>
2023-08-20 11:43:04 +0200

    add pagination

--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,21 +1,47 @@
+"use client";
 import { Recipe } from "@prisma/client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
-import { ArrowRight, Clock, Heart, Users2 } from "lucide-react";
+import { ArrowRight, Clock, Users2, ArrowLeft } from "lucide-react";
 
 interface Props {
   recipes: Recipe[];
 }
 
 const Recipes = ({ recipes }: Props) => {
+  const itemsPerPage = 40;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastRecipe = currentPage * itemsPerPage;
+  const indexOfFirstRecipe = indexOfLastRecipe - itemsPerPage;
+  const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+  const totalPages = Math.ceil(recipes.length / itemsPerPage);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   return (
     <>
       <h1 className="font-secoundary text-2xl lg:text-3xl font-medium">
         Recipes ({recipes.length})
       </h1>
       <div className="w-full grid grid-cols-2 md:grid-cols-4 gap-4 gap-x-6 grid-rows-2 md:grid-rows-1">
-        {recipes.map((recipe, index) => (
+        {currentRecipes.map((recipe, index) => (
           <Link key={index} href={`/recipe/${recipe.id}`}>
             <div className="flex flex-col bg-white border border-rose rounded-md">
               <Image
@@ -42,6 +68,33 @@ const Recipes = ({ recipes }: Props) => {
             </div>
           </Link>
         ))}
+      </div>
+      <div className="flex justify-center mt-4 mb-24">
+        <button
+          onClick={handlePrevPage}
+          className="mx-1 px-3 py-1 rounded-md bg-white text-rose"
+        >
+          <ArrowLeft className="inline-block w-4 h-4" />
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button
+            key={index}
+            onClick={() => handlePageChange(index + 1)}
+            className={`mx-1 px-3 py-1 rounded-md ${
+              currentPage === index + 1
+                ? "bg-rose text-white"
+                : "bg-white text-rose"
+            }`}
+          >
+            {index + 1}
+          </button>
+        ))}
+        <button
+          onClick={handleNextPage}
+          className="mx-1 px-3 py-1 rounded-md bg-white text-rose"
+        >
+          <ArrowRight className="inline-block w-4 h-4" />
+        </button>
       </div>
     </>
   );
